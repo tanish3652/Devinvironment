948036ecf2caf8fd5f1fa53b9e7be125d321b0de
tanish3652 <tanish3652@gmail.com>
2023-11-16 20:03:58 +0530

    make function

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 # print("hello world")
-name=input("please enter your name ")
-print("hello "+name)
+#name=input("please enter your name ")
+#print("hello "+name)
 
+def firstFunO:
+    print("I am happy")
+firstFunO
